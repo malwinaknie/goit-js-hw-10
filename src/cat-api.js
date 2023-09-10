753da38ed43f249e2bcf44dafbbc68a87c450c3e
Malwina Knie <malwinaknie@wp.pl>
2023-09-10 17:02:16 +0200

    update

---
+++ src/cat-api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = 
+"live_5eKOKLAMjaP9dsN7Pk7ejrEVLFqCOonoZS4KQJR2iU92yMe6uRiMLxH7jS5EPh1r";
+
+function fetchBreeds() {
+    return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+        return response.data;
+    })
+    .catch(error => {
+        throw new error('Oops! Something went wrong! Try reloading the page!');
+    })
+};
+
+function fetchCatByBreed(breedId) {
+    return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => {
+        return response.data;
+    })
+    .catch(error => {
+        throw new error('Oops! Something went wrong! Try reloading the page!');
+    })
+}
+
+export {fetchBreeds, fetchCatByBreed};
